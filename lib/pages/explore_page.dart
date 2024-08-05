b10aac2e61e61c14088b0f78ce77dd92467c8c62
Aviral Sharma <imaviralsharma@gmail.com>
2024-08-05 08:50:02 +0530

    Explore app bar

--- lib/pages/explore_page.dart
+++ lib/pages/explore_page.dart
@@ -4,16 +4,33 @@ class ExplorePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'This is the Explore page',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.more_vert, color: Colors.white),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Container(
+        color: Colors.black,
+        child: Center(
+          child: Text(
+            'Explore Page Content',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
           ),
         ),
       ),
-      backgroundColor: Colors.black,
     );
   }
 }
