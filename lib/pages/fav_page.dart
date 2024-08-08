88fb8d317e1868deb5fdf359de610abdf9a25b0e
Aviral Sharma <imaviralsharma@gmail.com>
2024-08-08 23:51:30 +0530

    Fav Page -1

--- lib/pages/fav_page.dart
+++ lib/pages/fav_page.dart
@@ -4,16 +4,26 @@ class FavPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'This is the Fav page',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-          ),
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
         ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.more_vert, color: Colors.white),
+            onPressed: () {},
+          ),
+        ],
       ),
       backgroundColor: Colors.black,
+      body: Container(
+        color: Colors.black,
+        // Add the rest of the content here
+      ),
     );
   }
 }
