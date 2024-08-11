f3bee3e3ce5b7c914c33535236238f30babad63a
Aviral Sharma <imaviralsharma@gmail.com>
2024-08-11 14:17:39 +0530

    Fav Page Final

--- lib/pages/fav_page.dart
+++ lib/pages/fav_page.dart
@@ -127,9 +127,113 @@ class FavPage extends StatelessWidget {
                 ],
               ),
             ),
+            Expanded(
+              child: ListView(
+                children: [
+                  _buildSongTile(
+                    'lib/images/1.jpg',
+                    'Wack we A wack',
+                    'Aviral',
+                    '3:45',
+                  ),
+                  _buildSongTile(
+                    'lib/images/2.jpg',
+                    'Major Lazer',
+                    'Daksh Bhaiya',
+                    '4:12',
+                  ),
+                  _buildSongTile(
+                    'lib/images/3.jpg',
+                    'Doh Wanna Go Bad',
+                    'Shinjan Bhaiya',
+                    '2:56',
+                  ),
+                  _buildSongTile(
+                    'lib/images/4.jpg',
+                    'No Problem',
+                    'Bhushan Bhaiya',
+                    '3:21',
+                  ),
+                  _buildSongTile(
+                    'lib/images/5.jpg',
+                    'Etana wanna go bad',
+                    'Vedant',
+                    '4:05',
+                  ),
+                  _buildSongTile(
+                    'lib/images/6.jpg',
+                    'Major Lazer',
+                    'Anish',
+                    '3:33',
+                  ),
+                  _buildSongTile(
+                    'lib/images/7.jpg',
+                    'Doh Wanna Go Bad',
+                    'Vedant',
+                    '3:40',
+                  ),
+                  _buildSongTile(
+                    'lib/images/8.jpg',
+                    'Wack we A wack',
+                    'Anish',
+                    '2:50',
+                  ),
+                  _buildSongTile(
+                    'lib/images/9.jpg',
+                    'Etana wanna go bad',
+                    'Shinjan Bhaiya',
+                    '3:15',
+                  ),
+                  _buildSongTile(
+                    'lib/images/10.jpg',
+                    'No Problem',
+                    'Daksh Bhaiya',
+                    '4:00',
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildSongTile(
+      String imagePath, String songTitle, String artistName, String time) {
+    return Column(
+      children: [
+        ListTile(
+          leading: Image.asset(
+            imagePath,
+            width: 50,
+            height: 50,
+          ),
+          title: Text(
+            songTitle,
+            style: TextStyle(color: Colors.white),
+          ),
+          subtitle: Text(
+            artistName,
+            style: TextStyle(color: Colors.grey),
+          ),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                time,
+                style: TextStyle(color: Colors.grey),
+              ),
+              SizedBox(width: 8),
+              Icon(Icons.more_vert, color: Colors.white),
+            ],
+          ),
+        ),
+        Divider(
+          color: Colors.grey[700],
+          thickness: 0.5,
+        ),
+      ],
     );
   }
 }
